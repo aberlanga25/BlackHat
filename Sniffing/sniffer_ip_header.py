40da7baf2dc39e8570629e988d6baf0ff30544bb
aberlanga25 <berlanga2512@gmail.com>
2019-06-21 18:00:30 -0600

    Home

--- Sniffing/sniffer_ip_header.py
+++ Sniffing/sniffer_ip_header.py
@@ -3,6 +3,9 @@ from ctypes import *
 import socket
 import struct
 import sys
+import threading
+import time
+from netaddr import IPNetwork, IPAddress
 
 
 class IP(Structure):
@@ -55,14 +58,28 @@ class ICMP(Structure):
     def __init__(self, buffer):
         pass
 
+def udp_sender(subnet, msg):
+    time.sleep(5)
+    sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    for ip in IPNetwork(subnet):
+        try:
+            sender.sendto(msg,("%s" % ip, 65212))
+        except:
+            pass
 
 if __name__ == '__main__':
 
     # this should look familiar from the previous example
     if not len(sys.argv[1:]):
         host = "127.0.0.1"
+        subnet = "192.168.0.0/24"
     else:
         host = sys.argv[1]
+        subnet = sys.argv[2]
+
+    msg = "B"
+
     if os.name == "nt":
         socket_protocol = socket.IPPROTO_IP
     else:
@@ -74,6 +91,9 @@ if __name__ == '__main__':
 
     if os.name == "nt":
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    t = threading.Thread(target=udp_sender, args=(subnet, msg))
+    t.start()
 
     try:
         while True:
@@ -98,6 +118,10 @@ if __name__ == '__main__':
 
                 print("ICMP -> Type: %d Code: %d" % (icmp_header.type, icmp_header.code))
 
+                if icmp_header.code == 3 and icmp_header.type == 3:
+                    if IPAddress(ip_header.src_address) in IPNetwork(subnet):
+                        if raw_buffer[len(raw_buffer)-len(msg):] == msg:
+                            print("Host Up: %s" % ip_header.src_address)
 
     except KeyboardInterrupt:
         # if we're using Windows, turn off promiscuous mode
